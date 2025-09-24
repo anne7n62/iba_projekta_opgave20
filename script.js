4a2e29376de4b8150643e5a971203fb239819c4f
Annemette M <annemettemygind@gmail.com>
2025-09-24 19:46:55 +0200

    final upload

--- script.js
+++ script.js
@@ -1,54 +1,38 @@
-let knapErstat = document.getElementById("knapErstat");
+const knapErstat = document.getElementById("knapErstat");
 
 knapErstat.addEventListener("click", function () {
-  //Hent referencer
-  let tekstfeltInput = document.getElementById("textarea");
-  let findInput = document.getElementById("find-tekst");
-  let erstatInput = document.getElementById("erstat-tekst");
-  let resultatTekst = document.querySelector("#result-text p");
-  const statusBesked = document.querySelector(".statusbesked");
+  //Hent html elementer
+  const tekstfeltInput = document.getElementById("textarea");
+  const findInput = document.getElementById("find-tekst");
+  const erstatInput = document.getElementById("erstat-tekst");
+  const resultatTekst = document.querySelector("#result-text p");
+  const statusBesked = document.getElementByID("statusbesked");
 
-  // Hent string values
-  let tekstfelt = tekstfeltInput.value;
+  // Hent tekst
+  const tekstfelt = tekstfeltInput.value;
   const findTekst = findInput.value;
   const erstatTekst = erstatInput.value;
 
-  // // Tjek om søgeteksten kun er bogstaver (inkl. æøåÆØÅ)
-  // const erOrd = /^[a-zA-ZæøåÆØÅ]+$/.test(findTekst);
-
-  // // Byg regex med eller uden ordgrænser
-  // const regex = erOrd
-  //   ? new RegExp(`\\b${escapeRegex(findTekst)}\\b`, "gi")
-  //   : new RegExp(escapeRegex(findTekst), "gi");
-
-  // // Escape specialtegn
-  // function escapeRegex(tekst) {
-  //   return tekst.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
-  // }
-
-  // Lav en RegExp baseret på søgeteksten.
-  // (g = global, i = ignore case)
-
+  // Regex baseret på inputtekst
   const regex = new RegExp(`\\b${findTekst}\\b`, "gi");
+  console.log("regex:", regex);
 
-  // Tæller matches - returnerer array
+  // Tæller matches
   const matches = tekstfelt.match(regex);
-  console.log("matches:", matches);
+  const antal = matches ? matches.length : 0;
 
-  // Viser hvor mange udskiftninger, der er blevet foretaget
-  // const antal = matches ? matches.length : 0;
-  const antal = matches.length;
+  console.log("matches:", matches);
   console.log("antal matches:", antal);
 
   if (antal === 0) {
     statusBesked.textContent = "Nul matches – ingen ændringer foretaget";
   } else if (antal === 1) {
-    statusBesked.textContent = `Teksten blev ændret 1 gang`;
+    statusBesked.textContent = "Teksten blev ændret 1 gang";
   } else {
     statusBesked.textContent = `Teksten blev ændret ${antal} gange`;
   }
 
-  // Erstatte matches med erstatningsteksten
+  // Erstatte regex tekst med input tekst
   const nyTekst = tekstfelt.replaceAll(regex, erstatTekst);
   console.log("Ny tekst:", nyTekst);
 
